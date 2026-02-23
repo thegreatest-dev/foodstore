06a9c30a29403c71ca8cf56bde70522e9b5e8d18
Akin-Olutegbe <danielakin557@gmail.com>
2026-02-23 13:05:31 +0100

    losks

--- app/page.tsx
+++ app/page.tsx
@@ -267,7 +267,7 @@ export default async function Home() {
           </p>
         </div>
 
-        <DealOfTheWeek products={dealProducts} />
+        <DealOfTheWeek products={dealProducts} allProducts={allProducts} />
 
         {/* Navigation Arrows */}
         <div className="flex justify-center gap-2 mt-8">
